4cbe6edd4c940c381038392ce57328bce244f343
root <root@golden.local>
2021-09-20 12:19:38 -0400

    Update sources

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
+  pkgs = import ./pkgs.nix { inherit sources; };
   extra-container = pkgs.callPackage sources.extra-container { };
+  niv = import sources.niv { inherit sources pkgs; };
 in
 with pkgs;
 mkShell {
-  nativeBuildInputs = [ extra-container nixos-container ];
+  nativeBuildInputs = [ extra-container nixos-container niv.niv ];
   shellHook = ''
     export NIX_PATH="nixpkgs=${sources.nixpkgs}"
     export LOCALE_ARCHIVE_2_27=${pkgs.glibcLocales}/lib/locale/locale-archive
