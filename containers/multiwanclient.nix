c8384880eda9b7aec54f6aac591330318e73c667
root <root@golden.local>
2021-10-10 16:56:56 -0400

    Add multiwan client

---
+++ containers/multiwanclient.nix
@@ -0,0 +1,28 @@
+{ pkgs
+}:
+let
+  mv_nic = "multiwan";
+in
+{
+  autoStart = true;
+  macvlans = [ mv_nic ];
+  nixpkgs = pkgs.path;
+  config = { ... }: {
+    imports = [
+      ../includes/common.nix
+      ../includes/client.nix
+    ];
+    nixpkgs.pkgs = pkgs;
+    networking.firewall.enable = false;
+    networking.useHostResolvConf = false;
+    systemd.network = {
+      enable = true;
+      networks."mv-${mv_nic}" = {
+        matchConfig = {
+          Name = "mv-${mv_nic}";
+        };
+        DHCP = "yes";
+      };
+    };
+  };
+}
