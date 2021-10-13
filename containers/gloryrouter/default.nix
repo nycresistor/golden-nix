59cda651d1da6afacc72ab2d49c111bdfb4be462
George Shammas <george@shamm.as>
2021-10-13 12:50:11 -0400

    gloryrouter container init

---
+++ containers/gloryrouter/default.nix
@@ -0,0 +1,48 @@
+{ pkgs }:
+{
+  autoStart = true;
+  macvlans = [ "enp2s0" "nycmesh" "linknyc" ];
+  nixpkgs = pkgs.path;
+
+  config = { ... }: {
+
+    nixpkgs.pkgs = pkgs;
+    imports = [
+      ../../includes/common.nix
+      ../../includes/client.nix
+    ];
+    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+    networking.useHostResolvConf = false;
+
+    systemd.network =
+      let
+        tabledInterface = iface: table: {
+          matchConfig = {
+            Name = iface;
+          };
+          DHCP = "yes";
+          routingPolicyRules = [
+            {
+              routingPolicyRuleConfig = {
+                OutgoingInterface = iface;
+                Table = table;
+              };
+            }
+          ];
+          dhcpV4Config = {
+            RouteTable = table;
+          };
+          ipv6AcceptRAConfig = {
+            RouteTable = table;
+          };
+        };
+      in
+      {
+        enable = true;
+        networks.mv-enp2s0 = tabledInterface "mv-enp2s0" 51;
+        networks.mv-nycmesh = tabledInterface "mv-nycmesh" 52;
+        networks.mv-linknyc = tabledInterface "mv-linknyc" 53;
+      };
+  };
+}
+
