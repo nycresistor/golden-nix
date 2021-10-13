382ae8329416478fb5ecb5076da8a9c00b513caa
George Shammas <george@shamm.as>
2021-10-13 15:25:49 -0400

    Add glorytun network config

--- containers/gloryrouter/default.nix
+++ containers/gloryrouter/default.nix
@@ -16,6 +16,51 @@
     networking.useNetworkd = true;
     networking.useHostResolvConf = false;
 
+    systemd.services."glorytun@gtc-main" = {
+
+      description = "A bridge between Matrix and Discord.";
+
+      wantedBy = [ "network-online.target" ];
+
+
+      serviceConfig =
+        let
+          ExecPost = pkgs.writeScript "glorytun-post.sh" ''
+            DEV=$1
+            getSourceIP() {
+              ip route get oif "$1" 172.104.15.252
+            }
+            for i in mv-enp2s0 mv-nycmesh mv-linknyc; do 
+              SRC=$(getSourceIP $i)
+              ${pkgs.glorytun}/bin/glorytun path up "$SRC" dev "$DEV" rate rx 12500000 tx 12500000
+            done
+            exit 0
+          '';
+        in
+        {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = 600;
+
+          ProtectSystem = "strict";
+          ProtectHome = true;
+          ProtectKernelTunables = true;
+          ProtectKernelModules = true;
+          ProtectControlGroups = true;
+          CapabilityBoundingSet = "CAP_NET_ADMIN";
+
+          PrivateTmp = true;
+          ExecStart = ''
+            ${pkgs.glorytun}/bin/glorytun bind 0.0.0.0 to 172.104.15.252 dev %i keyfile /root/glory.key chacha
+          '';
+          postStart = ''
+            "${ExecPost}" %s
+          '';
+
+        };
+
+    };
+
     systemd.network =
       let
         tabledInterface = iface: table: {
@@ -47,6 +92,21 @@
         networks.mv-enp2s0 = tabledInterface "mv-enp2s0" 51;
         networks.mv-nycmesh = tabledInterface "mv-nycmesh" 52;
         networks.mv-linknyc = tabledInterface "mv-linknyc" 53;
+        networks.gtc-main = {
+          matchConfig = {
+            Name = "gtc-main";
+          };
+          gateway = [ "192.168.91.1" ];
+          dns = [ "8.8.8.8" "1.1.1.1" ];
+          addresses = [
+            {
+              addressConfig = {
+                Address = "192.168.91.2";
+                Peer = "192.168.91.1";
+              };
+            }
+          ];
+        };
       };
   };
 }
